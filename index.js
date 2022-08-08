dfdd62d0ce2f6b4e0c47e22c7ff7074902edb69c
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-08-08 20:23:58 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,56 @@
+num1 = 75;
+num2 = 5;
+// to add 
+function add(num1, num2) {
+    return num1 + num2;
+}
+//to divide
+function divide(num1, num2) {
+    return num1/num2;
+}
+//to multiply
+function multiply() {
+    return num1 * num2;
+}
+//to divide
+function divide() {
+    return num1/num2;
+}
+
+number = 10;
+function add5() {
+    return number+=5 ;
+}
+function divideBy6() {
+    return number /= 6;
+}
+function multiplyBy4() {
+    return number *= 4;
+}
+function subtractBy3() {
+    return number -= 3;
+}
+
+
+number = 5;
+// increment function
+
+function increment(n) {
+    return n+1;
+}
+
+//decrement function
+function decrement(n){
+    return n-1;
+}
+//final part
+string = "5.88778"
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+
+function preserveDecimal(string) {
+    return parseFloat(string, 10);
+}
+makeInt(string);
